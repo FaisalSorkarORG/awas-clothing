e9abfd142a5d0dd358447fdb1f4ed2e8d143657b
FaisalSorkar <faisalsorkarorg@gamil.com>
2022-10-04 23:50:54 -0700

    updating our app with app with collection-preciew and collection-item commponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/ShopPage.component';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// const HatsPage = () => (
+//   <div>
+//     <h1>Hats Page</h1>
+//   </div>
+// )
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/shop' element= {<ShopPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
